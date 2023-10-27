2e62ba8149512a2a1960a1513e3570be5a70bf7e
Sanket Singh <singhsanket143@gmail.com>
2023-10-27 22:16:38 +0530

    Added eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.css';
 
 function App() {
 
@@ -6,7 +6,7 @@ function App() {
     <div>
       BMM
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
