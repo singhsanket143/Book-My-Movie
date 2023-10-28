31f02dc2e8603c71f691592714eb8cb0a3c9bb81
Sanket Singh <singhsanket143@gmail.com>
2023-10-28 20:10:21 +0530

    Added home page card posters

---
+++ src/Components/HomeMovieCard.tsx
@@ -0,0 +1,23 @@
+import { AiFillStar } from "react-icons/ai";
+
+type HomeMovieCardProps = {
+    movieImage: string
+}
+
+function HomeMovieCard({ movieImage } : HomeMovieCardProps) {
+    return (
+        <div className="flex flex-col items-center justify-center h-[416px] w-[222px]">
+            <img 
+                src={movieImage} 
+                className="h-[95%] w-full rounded-tl-lg rounded-tr-lg"
+            />
+            <div className="bg-black w-full text-white text-left rounded-br-lg rounded-bl-lg py-1">
+                <AiFillStar className="inline text-2xl text-red-500 mx-2" />
+                <span>9/10</span> {" "}
+                <span>52.5K Votes</span>
+            </div>
+        </div>
+    );
+}
+
+export default HomeMovieCard;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,20 @@
+import MoviePoster from '../Assets/movieposter.png';
 import HomeCarousel from "../Components/HomeCarousel";
+import HomeMovieCard from "../Components/HomeMovieCard";
 import Navbar from "../Components/Navbar";
-
 function Home() {
     return (
         <>
             <Navbar />
             <HomeCarousel />
+
+            <div className="mt-8 flex justify-center items-center gap-10">
+                <HomeMovieCard movieImage={MoviePoster} />
+                <HomeMovieCard movieImage={MoviePoster} />
+                <HomeMovieCard movieImage={MoviePoster} />
+                <HomeMovieCard movieImage={MoviePoster} />
+
+            </div>
             Home
         </>
     );
