20b2b237cbf8b833008b4458539a7c65dc44d95c
Sanket Singh <singhsanket143@gmail.com>
2023-11-15 22:52:11 +0530

    Seat selection logic implemented

--- src/Pages/SeatConfig.tsx
+++ src/Pages/SeatConfig.tsx
@@ -11,16 +11,42 @@ type Seat = {
 
 type Row = {
     number: string,
-    seats: [Seat]
+    seats: Seat[]
 }
 
-type SeatState = [Row];
+type SeatState = Row[];
 
 function SeatConfig() {
 
     const {state} = useLocation();
 
     const [seats, setSeats] = useState<SeatState>([{number: "", seats: [{number: 0, status: 0}]}]);
+
+    function processSeatColor(seat: Seat) {
+        if(seat.status == 0) return '';
+        else if(seat.status == 1) return 'border border-green-300 hover:bg-green-300';
+        else if(seat.status == 3) return 'border border-yellow-300 bg-yellow-300';
+        else return 'border border-gray-400 bg-gray-300';
+    }
+
+    function processSeatSelection(seatId: string) {
+        const [rowNumber, seatNumber] = seatId.split("-");
+        const updatedSeats = seats.map((row: Row) => {
+            if(row.number != rowNumber) return row;
+            else {
+                const updatedRowSeats = row.seats.map((seat: Seat) => {
+                    if(seat.number != +seatNumber) return seat;
+                    else {
+                        seat.status = (seat.status == 1) ? 3 : 1;
+                        return seat;
+                    }
+                });
+                row.seats = updatedRowSeats;
+                return row;
+            }
+        });
+        setSeats(updatedSeats);
+    }
 
     useEffect(() => {
         const seatsConfigJson = processSeatConfig(state.config);
@@ -31,6 +57,38 @@ function SeatConfig() {
         <HomeLayout>
          config
          {seats.length}
+         <div className="flex flex-col items-center justify-center p-16">
+            {
+                seats.map((row: Row) => {
+                    return (
+
+                        <div key={row.number} className="flex gap-8">
+
+                            <div>{row.number}</div>
+
+                            <div className="flex gap-2 my-2 mx-1">
+                                {row.seats.map((seat: Seat) => {
+
+                                    return (
+                                            seat.status == 0 ? 
+                                                (
+                                                <div className="h-[2rem] w-[2rem]"></div>
+                                                ) : 
+                                                (
+                                                    <div key={`${row.number}-${seat.number}`} onClick={() => processSeatSelection(`${row.number}-${seat.number}`)} className={`${processSeatColor(seat)} px-3 py-1 h-[2rem] w-[2rem]`}>
+                                                        {seat.number}
+                                                    </div>
+                                                )
+                                    );
+                                })}
+                            </div>
+
+                        </div>
+
+                    );
+                })
+            }
+         </div>
         </HomeLayout>
     );
 }
